095ec8df0cf713805b633cbeaf9b74c5d6813dcb
JosMiguelMM2 <josemmendez@ucundinamarca.edu.co>
2023-05-05 23:05:29 -0500

    Se modifica la base de datos, se ingresan datos a la base, se generaa la copia, se genera los archivos .csv

---
+++ Database/sql/insertar_datos_prueba/affectation.sql
@@ -0,0 +1,17 @@
+insert into public."GradeAffectation" ("Name", "Description")
+values ('Mínima', 'Casi ninguna afectación al medio ambiente.');
+
+insert into public."GradeAffectation" ("Name", "Description")
+values ('Baja', 'Afectación leve y reversible, con impacto mínimo en los ecosistemas.');
+
+insert into public."GradeAffectation" ("Name", "Description")
+values ('Moderada', 'Afectación moderada con posibles consecuencias a corto plazo.');
+
+insert into public."GradeAffectation" ("Name", "Description")
+values ('Significativa', 'Afectación significativa con impacto visible en los ecosistemas y en la calidad del medio ambiente.');
+
+insert into public."GradeAffectation" ("Name", "Description")
+values ('Alta', 'Afectación alta con consecuencias a largo plazo y posibles daños irreversibles.');
+
+insert into public."GradeAffectation" ("Name", "Description")
+values ('Grave', 'Afectación grave con impacto masivo y daños irreparables al medio ambiente.');
