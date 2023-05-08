52d30756a779fdb77348ff3dbc247e4fda2123bd
Daniel Valencia <danielvalenbf@gmail.com>
2023-05-08 00:50:33 -0500

    fix files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CorsModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [CorsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // TODO: Agregar direccion del front al cors
+  app.enableCors({
+    origin: 'http://localhost:8000',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
