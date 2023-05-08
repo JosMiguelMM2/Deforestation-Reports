f12a1837b672daa7f8ff2145c6c3cec3459c631c
Daniel Valencia <danielvalenbf@gmail.com>
2023-05-08 01:25:24 -0500

    fix issues

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import * as Joi from 'joi';
-
-
 @Module({
   imports: [],
   controllers: [AppController],
