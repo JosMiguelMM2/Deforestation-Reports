988bdfad64539c9a896d43182a762ca69485e298
JosMiguelMM2 <josemmendez@ucundinamarca.edu.co>
2023-05-16 00:08:26 -0500

    Solucionado

--- frontend-haltoforest/src/app/app-routing.module.ts
+++ frontend-haltoforest/src/app/app-routing.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './paginas/home/home.component';
-import { FormulariosComponent } from './pagina/formularios/formularios.component';
-import { MapasComponent } from './paginas/mapas/mapas.component';
-import { MapasGoogleComponent } from './paginas/mapas-google/mapas-google.component';
-import { MapasMapboxComponent } from './paginas/mapas-mapbox/mapas-mapbox.component';
 import { LoginComponent } from './Users/login/login.component';
 import { LoginUserComponent } from './Users/login-user/login-user.component';
 import { SingUpComponent } from './Users/sing-up/sing-up.component';
@@ -14,10 +10,6 @@ import { ApiMapsComponent } from './paginas/api-maps/api-maps.component';
 const routes: Routes = [
 
   {path:"",component:HomeComponent},
-  {path: "formularios",component:FormulariosComponent},
-  {path: "mapas",component:MapasComponent},
-  {path: "mapas/google",component:MapasGoogleComponent},
-  {path: "mapas/mapbox",component:MapasMapboxComponent},
   {path: "login",component:LoginComponent},
   {path: "login-user",component: LoginUserComponent},
   {path: "sing-up",component: SingUpComponent},
